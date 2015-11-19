4366b637b57b2e823c04c8007945ee2dee5c3fb9
renataribeiroreis <renataribeiroreis@hotmail.com>
2015-11-19 19:18:38 -0200

    Update Import.hs

--- Import.hs
+++ Import.hs
@@ -1,1 +1,21 @@
+{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+module Import where
 
+import Yesod
+ 
+pRoutes = [parseRoutes|
+
+    / InicioR GET
+    /page PageR GET 
+    /cadastro CadastroR GET POST
+    /listar ListarR GET
+   /pessoa/#PessoaId PessoaR GET
+   /depto DeptoR GET POST
+  /usuario UsuarioR GET POST
+  /professor ProfessorR GET POST
+  /campus CampusR GET POST
+  
+   
+   
+   
+|]
